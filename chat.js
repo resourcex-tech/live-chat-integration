2becd42f0c83266f570062a21c1035fbd072ef84
Amin <aminmajid.dev@gmail.com>
2024-05-30 07:07:50 +0500

    init

---
+++ chat.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    let container = document.querySelector("body");
+    const iframe = document.createElement("iframe");
+    iframe.src = "http://localhost:3008";
+    iframe.style = "border: 0; position: absolute; right:50px; bottom:0;";
+
+    // contentHeight sets an arbitrary default
+    // then keeps track of the last size update
+    // to avoid setting height again if nothing changes
+    let contentHeight = 500;
+    iframe.height = contentHeight;
+
+    window.addEventListener('load', () => {
+        container.appendChild(iframe);
+    });
+
+    window.addEventListener('message',
+        function (e) {
+            // message that was passed from iframe page
+            let message = e.data;
+
+            // before I update the height,
+            // I check to see if it's defined
+            // and if it's changed, and if
+            // it's not the iframe default
+            // of 150px
+            if (
+            message.height &&
+            message.height !== contentHeight &&
+            message.height !== 150
+            ) {
+            iframe.height = message.height + 'px';
+            contentHeight = message.height;
+            }
+            if (
+            message.width &&
+            message.width !== contentHeight &&
+            message.width !== 150
+            ) {
+            iframe.width = message.width + 'px';
+            width = message.width;
+            }
+        },
+        false
+    );
+})
---
+++ readme.md
@@ -0,0 +1,3 @@
+## Live Chat Integration Script
+
+Just include the js file in any of your projects and its ready to go.
